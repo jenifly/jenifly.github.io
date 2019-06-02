5369a2afc1d35cb0501f92e33a8ba6c223e68b7d
jenifly <1439916120@qq.com>
2019-06-02 13:31:55 +0800

    20190602

--- src/views/NoteBook.vue
+++ src/views/NoteBook.vue
@@ -6,10 +6,7 @@
 <script>
 export default {
   created () {
-    var fso=new ActiveXObject("Scripting.FileSystemObject")
-    var s=fso.GetFolder('/notes')
-    var fn=new Enumerator(s.files)
-    console.log(fn);
+    
     
   }
 }
